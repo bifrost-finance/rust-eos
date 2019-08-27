58fa0f553a753537701ac1949d7aa73e058c8187
Edwin Wang <lark930@gmail.com>
2019-08-27 19:25:50 +0800

    Some tests for keypair

--- keys/src/keypair.rs
+++ keys/src/keypair.rs
@@ -1,6 +1,5 @@
 #![allow(dead_code)]
 
-use secp256k1::{Secp256k1, Message};
 use rand::{CryptoRng, Rng};
 use crate::constant::*;
 use crate::error;
@@ -10,6 +9,7 @@ use crate::signature::Signature;
 
 
 /// A secp256k1 keypair.
+#[derive(Copy, Clone, PartialEq, Eq)]
 pub struct Keypair {
     /// The secret half of this keypair.
     pub sk: SecretKey,
@@ -18,19 +18,8 @@ pub struct Keypair {
 }
 
 impl Keypair {
-    /// Convert this keypair to bytes.
-    pub fn to_bytes(&self) -> [u8; KEYPAIR_LENGTH] {
-        let mut bytes: [u8; KEYPAIR_LENGTH] = [0u8; KEYPAIR_LENGTH];
-
-        bytes[..SECRET_KEY_SIZE].copy_from_slice(self.sk.to_bytes().as_slice());
-        bytes[SECRET_KEY_SIZE..].copy_from_slice(self.pk.to_bytes().as_slice());
-
-        bytes
-    }
-
     /// Generate an secp256k1 keypair.
     pub fn generate<R>(csprng: &mut R) -> Keypair where R: CryptoRng + Rng {
-        let secp = Secp256k1::new();
         let sk = SecretKey::generate(csprng);
         let pk = PublicKey::from(&sk);
 
@@ -45,6 +34,16 @@ impl Keypair {
         Ok(Keypair { sk, pk })
     }
 
+    /// Convert this keypair to bytes.
+    pub fn to_bytes(&self) -> [u8; KEYPAIR_LENGTH] {
+        let mut bytes: [u8; KEYPAIR_LENGTH] = [0u8; KEYPAIR_LENGTH];
+
+        bytes[..SECRET_KEY_SIZE].copy_from_slice(self.sk.to_bytes().as_slice());
+        bytes[SECRET_KEY_SIZE..].copy_from_slice(self.pk.to_bytes().as_slice());
+
+        bytes
+    }
+
     /// Sign a message with this keypair's secret key.
     pub fn sign(&self, message: &[u8]) -> Result<Signature, error::Error> {
         self.sk.sign(&message)
@@ -53,5 +52,49 @@ impl Keypair {
     /// Verify a signature on a message with this keypair's public key
     pub fn verify(&self, message: &[u8], signature: &Signature) -> Result<(), error::Error> {
         self.pk.verify(&message, &signature)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::rngs::OsRng;
+    use super::Keypair;
+    use crate::public::PublicKey;
+    use crate::secret::SecretKey;
+
+    #[test]
+    fn keypair_generate_should_work() {
+        let mut csprng: OsRng = OsRng::new().unwrap();
+        let keypair = Keypair::generate(&mut csprng);
+
+        assert_eq!(PublicKey::from(&keypair.sk), keypair.pk);
+    }
+
+    #[test]
+    fn keypair_from_secret_wif_should_work() {
+        let wif = "5HrBLKfeEdqH9KLMv1daHLVjrXV3DGVERAkN5cdSSc58bzqqfT4";
+        let keypair = Keypair::from_secret_wif(wif).unwrap();
+        assert_eq!(keypair.pk.to_string(), "EOS8FdQ4gt16pFcSiXAYCcHnkHTS2nNLFWGZXW5sioAdvQuMxKhAm");
+    }
+
+    #[test]
+    fn keypair_sign_should_work() {
+        let wif = "5HrBLKfeEdqH9KLMv1daHLVjrXV3DGVERAkN5cdSSc58bzqqfT4";
+        let keypair = Keypair::from_secret_wif(wif).unwrap();
+        let message = "hello".as_bytes();
+        let sig = keypair.sign(&message);
+        assert!(sig.is_ok());
+    }
+
+    #[test]
+    fn keypair_verify_should_work() {
+        let wif = "5HrBLKfeEdqH9KLMv1daHLVjrXV3DGVERAkN5cdSSc58bzqqfT4";
+        let keypair = Keypair::from_secret_wif(wif).unwrap();
+        let message = "hello".as_bytes();
+        let sig = keypair.sign(&message);
+        assert!(sig.is_ok());
+
+        let verify = keypair.verify(&message, &sig.unwrap());
+        assert!(verify.is_ok());
     }
 }
