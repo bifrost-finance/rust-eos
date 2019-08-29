83f9829e6a3c45d309d22036eae0545ca40bf857
Edwin Wang <lark930@gmail.com>
2019-08-29 16:53:43 +0800

    Signature from string

--- keys/src/signature.rs
+++ keys/src/signature.rs
@@ -1,10 +1,47 @@
 #![allow(dead_code)]
 use std::fmt;
-use crate::{base58, hash};
+use crate::{base58, hash, error};
+use std::str::FromStr;
 
 /// An secp256k1 signature.
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct Signature(pub secp256k1::recovery::RecoverableSignature);
+
+impl Signature {
+    pub fn is_canonical(sig: secp256k1::recovery::RecoverableSignature) -> bool {
+        let (_, pk) = sig.serialize_compact();
+
+        (pk[0] & 0x80 == 0)
+            && !((pk[0] == 0) && (pk[1] & 0x80 == 0))
+            && (pk[32] & 0x80 == 0)
+            && !((pk[32] == 0) && (pk[33] & 0x80 == 0))
+    }
+}
+
+impl FromStr for Signature {
+    type Err = error::Error;
+
+    fn from_str(s: &str) -> Result<Signature, error::Error> {
+        if !s.starts_with("SIG_K1_") {
+            return Err(error::Error::Secp256k1(secp256k1::Error::InvalidSignature));
+        }
+
+        let s_hex = base58::from(&s[7..])?;
+        let recid = match secp256k1::recovery::RecoveryId::from_i32((s_hex[0] - 4 - 27) as i32) {
+            Ok(recid) => recid,
+            Err(err) => return Err(err.into()),
+        };
+        // TODO verify with checksum
+        let data = &s_hex[1..65];
+        let checksum = &s_hex[65..];
+        let rec_sig = match secp256k1::recovery::RecoverableSignature::from_compact(&data, recid) {
+            Ok(rec_sig) => rec_sig,
+            Err(err) => return Err(err.into()),
+        };
+
+        Ok(Signature(rec_sig))
+    }
+}
 
 impl fmt::Display for Signature {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -28,5 +65,26 @@ impl fmt::Display for Signature {
         write!(f, "SIG_K1_{}", base58::encode_slice(&sig_slice))?;
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Signature;
+    use std::str::FromStr;
+
+    #[test]
+    fn sig_from_str_should_work() {
+        let sig_str = "SIG_K1_KomV6FEHKdtZxGDwhwSubEAcJ7VhtUQpEt5P6iDz33ic936aSXx87B2L56C8JLQkqNpp1W8ZXjrKiLHUEB4LCGeXvbtVuR";
+        let sig = Signature::from_str(sig_str);
+        assert!(sig.is_ok());
+        assert_eq!(sig.unwrap().to_string(), sig_str);
+    }
+
+    #[test]
+    fn sig_from_str_should_error() {
+        let sig_str = "KomV6FEHKdtZxGDwhwSubEAcJ7VhtUQpEt5P6iDz33ic936aSXx87B2L56C8JLQkqNpp1W8ZXjrKiLHUEB4LCGeXvbtVuR";
+        let sig = Signature::from_str(sig_str);
+        assert!(sig.is_err());
     }
 }
