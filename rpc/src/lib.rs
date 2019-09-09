591df77ac358ac15c152b558b7bd35aac961702c
Edwin Wang <lark930@gmail.com>
2019-09-09 19:23:26 +0800

    Rpc lib file

---
+++ rpc/src/lib.rs
@@ -0,0 +1,54 @@
+#![warn(
+    clippy::all,
+    clippy::complexity,
+    clippy::style,
+    clippy::perf,
+    clippy::nursery,
+    clippy::cargo
+)]
+
+pub mod chain;
+pub mod history;
+pub mod net;
+pub mod producer;
+
+mod client;
+mod clients;
+mod error;
+
+pub use self::client::*;
+pub use self::clients::*;
+pub use self::error::*;
+
+#[macro_export]
+macro_rules! builder {
+    ($path:expr, $params:ty, $output:ty) => {
+        impl $params {
+            pub fn fetch<C: crate::Client>(
+                self,
+                client: &C,
+            ) -> impl std::future::Future<Output = Result<$output, crate::Error>> {
+                client.fetch::<$output, $params>($path, self)
+            }
+        }
+    };
+}
+
+// mod builder {
+//     use crate::client::Client;
+//     use crate::error::Error;
+//     use futures::future::Future;
+//     use serde::{Deserialize, Serialize};
+
+//     pub trait Builder: Serialize {
+//         const PATH: &'static str;
+
+//         type Output: 'static + for<'de> Deserialize<'de>;
+
+//         fn fetch(&self, client: &Client) -> Box<Future<Item = Self::Output, Error = Error>> {
+//             Box::new(client.fetch(Self::PATH, Some(self)))
+//         }
+//     }
+// }
+
+// pub use self::builder::*;
