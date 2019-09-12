35e6e53a6c03d155f2167cff4feec5a536f9b8b1
Dengjianping <djptux@gmail.com>
2019-09-12 17:15:33 +0800

    removed the macro builder that has been replaced by rpc_codegen

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -19,36 +19,4 @@ mod error;
 pub use self::client::*;
 pub use self::clients::*;
 pub use self::error::*;
-
-#[macro_export]
-macro_rules! builder {
-    ($path:expr, $params:ty, $output:ty) => {
-        impl $params {
-            pub fn fetch<C: crate::Client>(
-                self,
-                client: &C,
-            ) -> impl std::future::Future<Output = Result<$output, crate::Error>> {
-                client.fetch::<$output, $params>($path, self)
-            }
-        }
-    };
-}
-
-// mod builder {
-//     use crate::client::Client;
-//     use crate::error::Error;
-//     use futures::future::Future;
-//     use serde::{Deserialize, Serialize};
-
-//     pub trait Builder: Serialize {
-//         const PATH: &'static str;
-
-//         type Output: 'static + for<'de> Deserialize<'de>;
-
-//         fn fetch(&self, client: &Client) -> Box<Future<Item = Self::Output, Error = Error>> {
-//             Box::new(client.fetch(Self::PATH, Some(self)))
-//         }
-//     }
-// }
-
-// pub use self::builder::*;
+pub use crate::chain::*;
