fbd4dd18476c29dbdb2ad5e24852d2dbfe10007f
Dengjianping <djptux@gmail.com>
2019-09-12 17:19:16 +0800

    removed some useless imported crates, and simplified the trait function fetch signatures

--- rpc/src/client.rs
+++ rpc/src/client.rs
@@ -1,17 +1,9 @@
-use crate::error::Error;
 use serde::{Deserialize, Serialize};
-use std::future::Future;
-use std::pin::Pin;
+
 
 pub trait Client {
     fn node(&self) -> &str;
 
-    fn fetch<Output, Params>(
-        &self,
-        path: &str,
-        params: Params,
-    ) -> Pin<Box<dyn Future<Output = Result<Output, Error>> + Send>>
-    where
-        Output: 'static + for<'b> Deserialize<'b> + Send,
-        Params: Serialize;
+    fn fetch<T>(&self, path: impl AsRef<str>, params: impl Serialize) -> crate::Result<T>
+        where T: 'static + for<'b> Deserialize<'b> + Send + Sync;
 }
