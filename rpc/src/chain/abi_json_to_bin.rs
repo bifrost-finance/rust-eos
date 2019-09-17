054fffa35a632981aab44ac8204e8b6c79b1a3f6
Dengjianping <djptux@gmail.com>
2019-09-17 19:24:04 +0800

    implemented api abi_json_to_bin

--- rpc/src/chain/abi_json_to_bin.rs
+++ rpc/src/chain/abi_json_to_bin.rs
@@ -1,13 +1,91 @@
-use eosio::{AccountName, ActionName};
-use serde::Serialize;
+use crate::Client;
+use eosio::{AccountName, ActionName, Symbol, n};
+use serde_derive::{Deserialize, Serialize};
+use rpc_codegen::Fetch;
 
-const PATH: &str = "/v1/chain/abi_json_to_bin";
 
-struct Params<Args>
-where
-    Args: Serialize,
+#[derive(Fetch, Clone, Debug, Deserialize, Serialize)]
+#[api(path="v1/chain/abi_json_to_bin", http_method="POST", returns="GetAbiJsonToBin")]
+pub struct GetAbiJsonToBinParams<Args: serde::Serialize>
 {
     code: AccountName,
     action: ActionName,
     args: Args,
 }
+
+pub fn get_abi_json_to_bin<Args: serde::Serialize>(
+    code: impl Into<AccountName>,
+    action: Actions,
+    args: Args
+) -> GetAbiJsonToBinParams<Args>
+{
+    let action: ActionName = match action {
+        Actions::Close => n!(close).into(),
+        Actions::Create => n!(create).into(),
+        Actions::Transfer => n!(transfer).into(),
+        Actions::Open => n!(open).into(),
+        Actions::Retire => n!(retire).into(),
+        Actions::Issue => n!(issue).into(),
+    };
+    GetAbiJsonToBinParams { code: code.into(), action, args }
+}
+
+// defined six action
+pub enum Actions {
+    Close,
+    Create,
+    Transfer,
+    Retire,
+    Open,
+    Issue
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct TransferAction {
+    pub from: AccountName,
+    pub to: AccountName,
+    pub quantity: String,
+    pub memo: String
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct CloseAction {
+    pub owner: AccountName,
+    pub symbol: String
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct CreateAction {
+    pub issuer: AccountName,
+    pub maximum_supply: String
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct IssueAction {
+    pub to: AccountName,
+    pub quantity: String,
+    pub memo: String
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct OpenAction {
+    pub owner: AccountName,
+    pub symbol: String,
+    pub ram_payer: AccountName
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct RetireAction {
+    pub quantity: String,
+    pub memo: String
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct GetAbiJsonToBin {
+    pub binargs: String
+}
+
+// #[cfg(test)]
+// mod test {
+//     // unimplemented!();
+// }
