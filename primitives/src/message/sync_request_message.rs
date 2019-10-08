fb47137f1d6fc2b884c28b537dd57cf513f40084
Edwin Wang <lark930@gmail.com>
2019-10-08 17:05:20 +0800

    sync request message

---
+++ primitives/src/message/sync_request_message.rs
@@ -0,0 +1,35 @@
+use crate::{Read, Write, NumBytes};
+
+#[derive(Clone, Debug, Read, Write, NumBytes, Default, PartialEq)]
+#[eosio_core_root_path = "crate"]
+pub struct SyncRequestMessage {
+    pub start_block: u32,
+    pub end_block: u32,
+}
+
+impl core::fmt::Display for SyncRequestMessage {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "\nstart_block: {}, end_block: {}\n",
+            self.start_block,
+            self.end_block
+        )
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::Read;
+    use hex;
+
+    #[test]
+    fn sync_request_message_test() {
+        let data = hex::decode("540000005f000000");
+        let data = data.unwrap();
+        let mut pos = 0usize;
+        let msg = SyncRequestMessage::read(&data.as_slice(), &mut pos);
+        println!("{}", msg.unwrap());
+        println!("Pos: {}", pos);
+    }
+}
+
