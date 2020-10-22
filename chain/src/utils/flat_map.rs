5c5b4492fcc93a99d061b6f3570181037e289a08
Dengjianping <djptux@gmail.com>
2020-10-22 17:34:49 +0800

    Modify flat_map type to solve deserialzie/serialzie problem

--- chain/src/utils/flat_map.rs
+++ chain/src/utils/flat_map.rs
@@ -7,32 +7,30 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Default, Encode, Decode, PartialEq)]
 #[cfg_attr(feature = "std", derive(Deserialize, Serialize))]
-pub struct FlatMap<K, V> {
-    maps: Vec<(K, V)>,
-}
+pub struct FlatMap<K, V>(
+    Vec<(K, V)>
+);
 
 impl<K, V> FlatMap<K, V> {
     pub fn new(key: K, value: V) -> Self {
-        Self {
-            maps: vec![(key, value)],
-        }
+        Self (
+            vec![(key, value)]
+        )
     }
 
     pub fn size(&self) -> UnsignedInt {
-        UnsignedInt::from(self.maps.len())
+        UnsignedInt::from(self.0.len())
     }
 
     pub fn assign(v: Vec<(K, V)>) -> Self {
-        Self {
-            maps: v
-        }
+        Self(v)
     }
 }
 
 impl<K: NumBytes, V: NumBytes> NumBytes for FlatMap<K, V> {
     fn num_bytes(&self) -> usize {
         let mut size = self.size().num_bytes();
-        for map in self.maps.iter() {
+        for map in self.0.iter() {
             size = size.saturating_add(map.num_bytes());
         }
 
@@ -43,7 +41,7 @@ impl<K: NumBytes, V: NumBytes> NumBytes for FlatMap<K, V> {
 impl<K: Write, V: Write> Write for FlatMap<K, V> {
     fn write(&self, bytes: &mut [u8], pos: &mut usize) -> Result<(), WriteError> {
         self.size().write(bytes, pos)?;
-        for map in self.maps.iter() {
+        for map in self.0.iter() {
             map.write(bytes, pos)?;
         }
 
@@ -61,6 +59,7 @@ impl <K: Read, V: Read> Read for FlatMap<K, V> {
             maps.push(map);
         }
 
-        Ok(FlatMap { maps })
+        // Ok(FlatMap { maps })
+        Ok(FlatMap(maps))
     }
 }
